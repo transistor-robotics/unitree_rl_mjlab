6138a8ab34934085e7335aa3fb24e6738d13b5c0
Eddie Caldwell <eddcaldwell@hotmail.com>
2026-02-19 12:33:01 +0900

    fixed weight and param field misplacement for ball height

--- mjlab/tasks/keepyup/keepyup_env_cfg.py
+++ mjlab/tasks/keepyup/keepyup_env_cfg.py
@@ -219,7 +219,7 @@ def make_keepyup_env_cfg() -> ManagerBasedRlEnvCfg:
             params={"sensor_name": "paddle_ball_contact"},
         ),
         "ball_height": RewardTermCfg(
-            func=mdp.ball_height_reward, weight=1.8, params={"target_height": 2.5}
+            func=mdp.ball_height_reward, weight=2.5, params={"target_height": 1.4}
         ),
         "bounce_rhythm": RewardTermCfg(
             func=mdp.bounce_rhythm_reward,
