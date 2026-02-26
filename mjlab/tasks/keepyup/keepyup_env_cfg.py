81c4024682a2f80aa6d164bc4ee6ad364ef224d8
Eddie Caldwell <eddcaldwell@hotmail.com>
2026-02-26 14:27:46 +0900

    trivialise curriculum to check for isseus

--- mjlab/tasks/keepyup/keepyup_env_cfg.py
+++ mjlab/tasks/keepyup/keepyup_env_cfg.py
@@ -300,14 +300,14 @@ def make_keepyup_env_cfg() -> ManagerBasedRlEnvCfg:
             params={
                 "event_term_name": "reset_arm_then_ball",
                 "reward_term_name": "total_bounces",
-                "promote_bounces": 1.0,
+                "promote_bounces": 0.0,
                 "rollback_bounces": 1.0,
-                "promote_threshold": 0.6,
+                "promote_threshold": 0.0,
                 "rollback_threshold": 0.3,
-                "promote_patience": 3,
+                "promote_patience": 1,
                 "rollback_patience": 3,
                 "ema_alpha": 0.1,
-                "min_episodes_per_window": 32,
+                "min_episodes_per_window": 1,
                 # Variances are normalized [0, 1] fractions of max spawn ranges.
                 # Spawn height is sampled in [1.6, min_spawn_height].
                 "stages": [
